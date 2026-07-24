e55552800560de497d41e4e3a0c4337d5f035a4f
Mario270creator <77266467+Mario270creator@users.noreply.github.com>
2026-07-24 13:29:26 +0300

    Delete static directory

--- static/legacy_lab/assets/pwa.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  if(!('serviceWorker' in navigator)) return;
-  if(!/^https?:$/i.test(window.location.protocol || '')) return;
-  window.addEventListener('load', function(){
-    navigator.serviceWorker.register('sw.js').catch(function(){});
-  });
-})();
